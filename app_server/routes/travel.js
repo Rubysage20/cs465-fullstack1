9d2e86b90570e98b4e98a8d367db6a6b1f0842b0
Valerie Dawson <135853594+Rubysage20@users.noreply.github.com>
2025-07-26 20:08:13 -0400

    Update travel.js

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/travel'); // Import the travel controller
+var controller = require('../controllers/travel'); 
 
 /* GET travel page. */
-router.get('/travel', controller.travel); // Use the travel method from the travel 
-
-module.exports = router; // Export the router
+router.get('/travel', controller.travel); 
+module.exports = router; 
 
